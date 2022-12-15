000293cb555001ed06b14f6a2ea28a4a6aaf89ae
malheiros <dmalheiros77@gmail.com>
2022-12-14 23:57:55 -0300

    finances

--- server/src/controllers/finances.js
+++ server/src/controllers/finances.js
@@ -6,5 +6,11 @@ module.exports = {
       if (error) throw error;
       return res.status(201).json({'msg': 'ok'});
     })
-  }
+  },
+
+  getFinancesByUser(req, res){
+    pool.query('SELECT * FROM financial_transaction WHERE id_user = ?', [req.query.user_id], function(error, results, fields){
+      if (error) throw error;
+      return res.json(results);
+    })} 
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const user = require('./controllers/user')
+const finances = require('./controllers/finances')
 
 const routes = express.Router();
 
 //rotas dos usuários
 routes.get('/users', user.getAllUsers);
 routes.post('/users', user.addUser);
+
+// rotas de financial_transaction
+routes.get('/finances', finances.getFinancesByUser)
+routes.post('/finances', finances.addFinance)
 
 module.exports = routes;

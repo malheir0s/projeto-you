d97bcc6745233fc9b40247ec64644ee712ca2d4f
Tiago Almeida <tiagoalmeidasaj@gmail.com>
2022-12-15 01:17:58 -0300

    DEV: rota de finanças

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const user = require('./controllers/user')
+const user = require('./controllers/user');
+const financialTransaction = require('./controllers/financialTransaction');
 
 const routes = express.Router();
 
 routes.get('/users', user.getAllUsers);
+
+routes.get('/financial_transaction/:idUser', financialTransaction.getTransctions);
+routes.get('/financial_transaction/count/is_expanse/:idUser', financialTransaction.countTransactionByExpense);
+routes.get('/financial_transaction/count/essential/:idUser', financialTransaction.countTransactionByEssential);
 
 module.exports = routes;
